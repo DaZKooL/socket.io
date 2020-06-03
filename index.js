1482a53fe562ae4206fd5cd50808c946dc4a88cb
g.grasso <g.grasso@carepay.com>
2020-06-03 10:43:30 +0200

    collab: cleaned up code

--- index.js
+++ index.js
@@ -4,64 +4,11 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
-const facemesh = require('@tensorflow-models/facemesh');
+const port = process.env.PORT || 3000
 
 
-async function main() {
-  // Load the MediaPipe facemesh model.
-  return;
-  const model = await facemesh.load();
- console.log (model);
-  // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain an
-  // array of detected faces from the MediaPipe graph.
-  const predictions = await model.estimateFaces(document.querySelector("video"));
- 
-  if (predictions.length > 0) {
-    /*
-    `predictions` is an array of objects describing each detected face, for example:
- 
-    [
-      {
-        faceInViewConfidence: 1, // The probability of a face being present.
-        boundingBox: { // The bounding box surrounding the face.
-          topLeft: [232.28, 145.26],
-          bottomRight: [449.75, 308.36],
-        },
-        mesh: [ // The 3D coordinates of each facial landmark.
-          [92.07, 119.49, -17.54],
-          [91.97, 102.52, -30.54],
-          ...
-        ],
-        scaledMesh: [ // The 3D coordinates of each facial landmark, normalized.
-          [322.32, 297.58, -17.54],
-          [322.18, 263.95, -30.54]
-        ],
-        annotations: { // Semantic groupings of the `scaledMesh` coordinates.
-          silhouette: [
-            [326.19, 124.72, -3.82],
-            [351.06, 126.30, -3.00],
-            ...
-          ],
-          ...
-        }
-      }
-    ]
-    */
- 
-    for (let i = 0; i < predictions.length; i++) {
-      const keypoints = predictions[i].scaledMesh;
- 
-      // Log facial keypoints.
-      for (let i = 0; i < keypoints.length; i++) {
-        const [x, y, z] = keypoints[i];
- 
-        console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-      }
-    }
-  }
-}
-setTimeout (main,3000);
+//tf.setBackend('wasm').then(() => main());
+
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
   
