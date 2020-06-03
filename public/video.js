3d6e76567b95237805e3598a85fbb7b691db2bc4
g.grasso <g.grasso@carepay.com>
2020-06-03 14:26:43 +0200

    collab: new setup

--- public/video.js
+++ public/video.js
@@ -21,7 +21,7 @@ import * as tf from './@tensorflow/tfjs-core';
 import * as tfjsWasm from './@tensorflow/tfjs-backend-wasm';
 // TODO(annxingyuan): read version from tfjsWasm directly once
 // https://github.com/tensorflow/tfjs/pull/2819 is merged.
-import {version} from './@tensorflow/tfjs-backend-wasm/dist/version';
+import {version} from '.  /@tensorflow/tfjs-backend-wasm/dist/version';
 
 import {TRIANGULATION} from './triangulation';
 
